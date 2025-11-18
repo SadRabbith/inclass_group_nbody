9eab84a9fe67ad439996f91defefb0f5411c86e3
sadman raabith <sraabi1@lsu.edu>
2025-11-18 01:03:25 -0500

    checks if particles move properly

--- checks.py
+++ checks.py
@@ -27,3 +27,16 @@ def test_particles_initialized_in_box():
     m_pos, _ = initialize_particles(M_pos, np.zeros(2), 200, box_size=10, R=0.2)
 
     assert np.all(m_pos >= 0) and np.all(m_pos <= 10), "Particles initialized outside simulation box."
+
+
+def test_big_particle_moves():
+    time, M_pos_history, M_vel_history, _ = run_simulation(
+        M=1.0, m=0.001, R=0.1, box_size=10.0, n_particles=10,
+        dt=0.01, total_time=0.1,
+        M_pos_init=np.array([2.0, 5.0]),
+        M_vel_init=np.array([1.0, 0.0])
+    )
+
+    #projectile should have changed its position
+    assert not np.allclose(M_pos_history[0], M_pos_history[-1]), \
+        "Projectile did not move during simulation."
